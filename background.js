c39962798122364085600c43f7c74c442bfe0d4e
cphamlet <cphamlet@live.unc.edu>
2017-12-01 01:38:52 -0500

    added one save popup at a time, added highlihging on different links

--- background.js
+++ background.js
@@ -2,18 +2,29 @@ var items = {
     objs : [] 
 };
 
+//This global variable is meant to maintain the button state across refreshed pages. For example, when you
+//advance to the next link, you will maintain the button on the bottom right.  
+var load_status = false; 
+
 //Define task object:
 
 //"element_html" : <html here>
 //"entered_text" : This is the text the professor entered
 
+//This background listener listens for comamnds from the content script. The commands are:
+//Get: this returns the first itme in the stack
+//Send: This adds an item to the stack
+//Save: For now all this does is return the entire stack jsonified. This will send to the server later
+//Clear: This empties all of the items in the list. (Say )
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     console.log(sender.tab ?
                 "from a content script:" + sender.tab.url :
                 "from the extension");
-    if (request.command == "get"){
-      sendResponse({msg: "Background: sending element from background script", taskObj:items.objs.shift()});
+    if (request.command == "peek"){
+      sendResponse({msg: "Background: sending element from background script", taskObj:items.objs[0]});
+    }else if(request.command == "shift"){
+      sendResponse({msg: "Background: sending element from background script, top item deleted", taskObj:items.objs.shift()});
     }else if (request.command == "send"){
     	items.objs.push({"element_html" :request.element_html, "entered_text": request.entered_text, "url": request.url});
     	console.log(request.element_html);
@@ -23,6 +34,11 @@ chrome.runtime.onMessage.addListener(
     }else if (request.command == "clear"){
       items = { objs : [] };
       sendResponse("CLEARED");
+    }else if(request.command == "get-load-status"){
+      sendResponse(load_status);
+    }else if(request.command == "set-load-status"){
+      load_status = !load_status;
+      sendResponse(load_status);
     }
 
 
--- main.js
+++ main.js
@@ -1,20 +1,57 @@
+/*
+* Author: Connor Hamlet
+* Funcitonality: This is the content script for our google chrome extension. 
+*
+*
+*
+*/
+//x and y of mouse pointer
 var x = 0;
 var y = 0;
+//This element is the element that currently have a border around it when it is selected. 
+var unborderedElementPointerHTML = null;
 $(window).mouseover(function(e) {
     x = e.clientX, y = e.clientY;
 });
 
 
 var student_view_next = "null";
+//This message asks the background if you are in "load mode"<p>Since the index is zero-based, the first list item is returned:</p>
 
+$(document).ready(function() {
+    retrieveItemfromBackgroundScript();
+});
 
-function create_popup_box(top, left, borderedElement){
+function retrieveItemfromBackgroundScript(){
+      chrome.runtime.sendMessage({command: "get-load-status"}, 
+                function(response) {
+                    //if false, you are not in "load mode"
+                     if(response == false){
+                        console.log("Not loaded.");
+                     }else{
+                        createAdvanceLinkButton();
+                        //Message asks background if you are on the correct link
+                        chrome.runtime.sendMessage({command: "peek"}, function(response) {
+                            //If equal, load the html 
+                            if(typeof response.taskObj != 'undefined' && window.location["href"] == response.taskObj["url"]) {
+                                //Message asks background for html and moves to next item. 
+                                chrome.runtime.sendMessage({command: "shift"}, function(responseShift) {
+                                    console.log("shifted:" + JSON.stringify(responseShift.taskObj));
+                                    loadHTMLContent(responseShift);
+                                }); //end of shift command msg 
+                            }//end of null check
+                        }); //end peek send msg
+                     } //end else
+                }); //end first reponse function
+    }
+
+//Creates the button for the "next" button 
+function create_popup_box(top, left, borderedElement, popup_ID){
 
 var new_offset = {top:top, left:left};
     var new_width = 200;
     var new_height = 50;
-
-    var created_element = $('<div class = "4k3jfn"></div>');
+    var created_element = $('<div class = "4k3jfn" id ="'+popup_ID+'" ></div>');
     var editable_text = $('<div contenteditable = "true" class ="56sdjfh"></div>');
     var save_button = $('<div class = "ck42jr"></div>');
     var button_text = $('<span class = "34ifun">Save</span>');
@@ -22,7 +59,9 @@ var new_offset = {top:top, left:left};
 
     editable_text.css({
         'background-color':'#ededed',
-        'height'    : '40px'
+        'height'    : '40px',
+        'font-size' : '12px',
+        'color'     : 'black'
     });
     save_button.css({
         'background-color' : '#00826c',
@@ -59,41 +98,52 @@ var new_offset = {top:top, left:left};
         .offset(new_offset).appendTo('body');
 
 
+        //When you click on the textbox, give keyboard focus. 
         $(created_element).click(function(){
             $(editable_text).focus();
         });
 
+
+        //Hanldes the click function for newly created save button
         $(save_button).click(function(){
-            borderedElement.style.border = "";
-           chrome.runtime.sendMessage({command: "send", element_html:elementOnMouseOver.outerHTML, entered_text:$(editable_text).text(), url:window.location["href"]}, 
+            //When you click the save button, this reverts the green block of text back to normal. 
+              if(unborderedElementPointerHTML!=null) {
+                  //This replace with function, removes the element with the green border if one already exists. 
+                  $(elementOnMouseOver).replaceWith($(unborderedElementPointerHTML).prop('outerHTML'));
+              }
+           chrome.runtime.sendMessage({command: "send", element_html:unborderedElementPointerHTML, entered_text:$(editable_text).text(), url:window.location["href"]}, 
             function(response) {
-                    alert(response.msg +" : "+ response.enteredText + " : "+ window.location["href"]);
+                //    alert(response.msg +" : "+ response.enteredText + " : "+ window.location["href"]);
             });
-
             created_element.remove();
-
-
-
-
-
         });
 
         return created_element;
 
-        };
+};
 
 
 $(document).keydown(function(event) {
     switch(event.which) {
         case 37: // left
-      //  alert("Entering Key Left");
+        //This popup id is the unique ID for all of the SAVE button popups. 
+                var popup_ID = "4iufbw";
+                //If a popup already exists, delete the old one. 
+                if(document.body.contains( document.getElementById(popup_ID) )){
+                    if(unborderedElementPointerHTML!=null) {
+                        //This replace with function, removes the element with the green border if one already exists. 
+                        $(elementOnMouseOver).replaceWith($(unborderedElementPointerHTML).prop('outerHTML'));
+                    }
+                    document.getElementById(popup_ID).remove();
+                }
         elementOnMouseOver = document.elementFromPoint(x, y);
-
+        unborderedElementPointerHTML = elementOnMouseOver.outerHTML;
             if(elementOnMouseOver.style.border == "" && elementOnMouseOver.tagName != "input"){
     			elementOnMouseOver.style.border = "thick solid green";
-                var edit_box = create_popup_box($(elementOnMouseOver).offset().top, $(elementOnMouseOver).offset().left+50, elementOnMouseOver);
-   			}
 
+                
+                var edit_box = create_popup_box($(elementOnMouseOver).offset().top, $(elementOnMouseOver).offset().left+50, elementOnMouseOver, popup_ID);
+   			}
                 break;
 
     }
@@ -101,36 +151,57 @@ $(document).keydown(function(event) {
 });
 
 function goToNextURL(){
-            chrome.runtime.sendMessage({command: "get"}, function(response) {
-            //alert(response.task_obj["element_html"]);
+            chrome.runtime.sendMessage({command: "peek"}, function(response) {
 
             //This sends the user to the next page if the urls do not match
-            if(window.location["href"] != response.taskObj["url"]) {
+            if(typeof(response.taskObj) != 'undefined' && window.location["href"] != response.taskObj["url"]) {
                 window.location = response.taskObj["url"];
-            }
-
-            
-            alert("Searching for element: " + response.taskObj["element_html"]);
-
-            var all_elements = document.getElementsByTagName("*");
-
-            for (var i = 0, element; element = all_elements[i++];) {
-                if(element.outerHTML == response.taskObj["element_html"]){
-                    alert("Item found: " + response.taskObj["entered_text"]);
-                    element.style.border = "thick solid green";
-                }
+            }else{
+                   loadHTMLContent(response);
             }
 
          });
 }
 
+//Displays border on webpage from element stored in background page. 
+function loadHTMLContent(responseObj){
+            console.log("Searching for element: " + responseObj.taskObj["element_html"]);
+            var all_elements = document.getElementsByTagName("*");
+            for (var i = 0, element; element = all_elements[i++];) {
+                console.log(element.outerHTML);
+                if(element.outerHTML == responseObj.taskObj["element_html"]){
+                    console.log("Item found: " + responseObj.taskObj["entered_text"]);
+                    element.style.border = "thick solid green";
+                }
+            }
+
+}
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.command== "load"){
-        if(student_view_next != "null")
-            student_view_next.remove();
+    if (request.command == "load"){
 
-    student_view_next = $('<div id = "fjh43jfb">Click me to go next</div>');
+        if(student_view_next != "null"){
+            student_view_next.remove();
+            student_view_next = "null";
+            sendResponse("Action completed");
+            return;
+        }
+
+    chrome.runtime.sendMessage({command: "set-load-status"}, 
+            function(response) {
+                    console.log("Set load status to: "+response);
+                 });
+    createAdvanceLinkButton();
+    sendResponse("Action completed");
+    }
+});
+
+function createAdvanceLinkButton(){
+    if(student_view_next != "null"){
+            student_view_next.remove();
+    }
+    student_view_next = $('<div id = "fjh43jfb">Next</div>');
 
     student_view_next.css({
         'height': '40px', 
@@ -138,7 +209,7 @@ chrome.runtime.onMessage.addListener(
         'bottom':'5%',
         'right':'5%',
         'width': '70px',
-        'background-color': '#393838',
+        'background-color': '#0080ff',
         'opacity': '1'
     });
 
@@ -148,8 +219,7 @@ chrome.runtime.onMessage.addListener(
         goToNextURL();
     });
 
-      sendResponse("Action completed");
-    }
-  });
+
+}
 
 
