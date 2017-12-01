72f17375f9197441b9970eaf7b292f78c5eda53b
cphamlet <cphamlet@live.unc.edu>
2017-12-01 01:38:08 -0500

    old updated propagating

--- popup.js
+++ popup.js
@@ -1,14 +1,12 @@
 $( document ).ready(function() {
 
-	function callIt(){
+	function removeLoginScreen(){
 		//send data to server
-		console.log("CAlled call it");
 		$("#main_screen").show();
 		$("#login_screen").hide();
-		
 	}
 
-	$("#login").click(callIt());
+	$("#login").click(removeLoginScreen());
 //Save record button sends to server
 	$("#save_record").click(function(){
 		//send data to server
@@ -31,11 +29,10 @@ $( document ).ready(function() {
 //Loads the record into the frame
 	$("#load_record").click(function(){
 	//send data to server
-	console.log("entering load record");
+	console.log("Entering load record function");
 		  chrome.tabs.query({active: true, currentWindow: true}, function(arrayOfTabs) {
 		     // since only one tab should be active and in the current window at once
 		     // the return variable should only have one entry
-		     console.log("THE TAB ID: "+ arrayOfTabs[0].id);
 		     chrome.tabs.sendMessage(arrayOfTabs[0].id, {command:'load'}, function(response) {
 			    console.log('Start action sent');
 			});//end send message
